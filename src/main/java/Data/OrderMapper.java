80eff01b913fb47dfe1f3e801d9bb587164ec973
yollysparks <yolanda.owiso.yo@gmail.com>
2017-07-06 12:49:34 +0200

    latest commit

--- src/main/java/Data/OrderMapper.java
+++ src/main/java/Data/OrderMapper.java
@@ -45,5 +45,32 @@ public  Order getOrderByCustomerID(int customerid)throws SQLException{
         }
       return order;  
     }
+ public Order getOrders() throws SQLException, ExceptionsThrown{
 
-  }
+            String querry=("SELECT * FROM carport.`order`;" );
+            Order order = null;
+            try{
+            con= Connector.getConnection();          
+            ResultSet res = Connector.doQuery(querry);
+            if(!res.next());
+            int idOrder = res.getInt("idOrder");
+            int payment = res.getInt("payment");
+            String date = res.getString("date");
+            int  customerid = res.getInt("customerid");
+           
+           order = new Order(idOrder,payment,date,customerid);
+             return order;
+            }catch (SQLException ex) {
+	       System.out.println(ex.getMessage());
+		} finally {
+	           if(con != null){
+                     try {
+                       con.close();
+                     }catch (SQLException ex) {
+                         throw new ExceptionsThrown("Error",ex);
+                     }
+		   }
+               }
+     return order; 
+    }   
+}
